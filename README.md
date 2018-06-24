6f445a0ca4134a45d42122e7be71e30498b6b3e8
Adrien Vergnaud <a.vergnaud@gmail.com>
2018-06-24 11:54:29 +0200

    mongodb

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # slack-commands-server
 
-curl -X POST -i -H "Content-Type:application/x-www-form-urlencoded" -d '{ "slackUser":"adrien", "wallet":"0x123" }' http://0.0.0.0:5001/users
+curl -X POST -F 'text=0x123' -F 'user_id=U8XXXXXXN' -F 'user_name=mister-gros-bot' http://0.0.0.0:5001/users
 
-curl -vX POST http://0.0.0.0:5001/users -d @/home/ubuntu/dev3/slack-commands-server/exemple.json --header "Content-Type:application/x-www-form-urlencoded"
+> db.users.find()
+{ "_id" : ObjectId("5b2f6447b252c61fd65be9db"), "wallet" : [ "0x123" ], "slack_user_id" : [ "U8XXXXXXN" ], "slack_user_name" : [ "mister-gros-bot" ] }
 
-curl -X POST -i -H "Content-Type:application/x-www-form-urlencoded" -d '{ "slackUser":"adrien", "wallet":"0x123" }' https://allstack.fr/slack-commands-server/users
+curl -X POST -F 'text=0x123' -F 'user_id=U8XXXXXXN' -F 'user_name=mister-gros-bot' https://allstack.fr/slack-commands-server/users
--- server/flask_app.py
+++ server/flask_app.py
@@ -1,28 +1,49 @@
 from flask import Flask, request
 from flask_cors import CORS
+from flask_pymongo import PyMongo
 import json
+from bson import json_util
 
+# conf flask
 app = Flask(__name__)
 CORS(app)
 
+# conf mongo
+app.config['MONGO_DBNAME'] = 'slack-users-db'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/slack-users-db'
+mongo = PyMongo(app)
+
 
 @app.route('/users', methods=['POST'])
-def user():
-    if request.method == 'POST':
-        data = request.form  # a multidict containing POST data
+def add_user():
+    # TODO vérifier le token pour sécuriser l'insertion
+    # https://api.slack.com/slash-commands "Validating the command"
 
-        form_dict = data.to_dict(flat=False)
-        print(form_dict)
+    # récupération des données
+    data = request.form
+    user_dict = data.to_dict(flat=False)
+    # insert
+    user_id = mongo.db.users.insert({"slack_user_id": user_dict["user_id"], "wallet_address": user_dict["text"],
+                                     "slack_user_name": user_dict["user_name"]})
+    new_user = mongo.db.users.find_one({'_id': user_id})
+    # réponse
+    retour = "user créé : " + json.dumps(new_user, default=json_util.default)
+    return retour, 200
 
-        form_json = json.dumps(form_dict)
-        print(form_json)
 
-        test_content = "commande bien reçue, payload : " + str(form_json)
+@app.route('/users', methods=['GET'])
+def get_user_by_wallet():
+    wallet_address = request.args.get('wallet_address')
+    user = mongo.db.users.find_one({'wallet_address': wallet_address})
+    if user is not None:
+        return json.dumps(user, default=json_util.default), 200
+    else:
+        return "error", 403
 
-        return test_content, 200
 
 def run():
     app.run(host='0.0.0.0', port=5001, debug=True)
+
 
 if __name__ == '__main__':
     run()
